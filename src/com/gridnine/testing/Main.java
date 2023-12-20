c801bfa86394a174d41f70fad80d5497e87f9ae1
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-12-20 22:17:39 +0300

    conditionTwo

--- src/com/gridnine/testing/Main.java
+++ src/com/gridnine/testing/Main.java
@@ -1,8 +1,35 @@
 package com.gridnine.testing;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        List<Flight> testList = FlightBuilder.createFlights();
+
+        List<Flight> conditionOneList = conditionOne(testList);
+        System.out.println(conditionOneList);
+
+        List<Flight> conditionTwoList = conditionTwo(testList);
+        System.out.println(conditionTwoList);
+
 
     }
+
+    private static List<Flight> conditionOne(List<Flight> testList) {
+        return testList.stream()
+                .filter(flight -> flight.getSegments()
+                        .stream().findFirst().get().getDepartureDate().isAfter(LocalDateTime.now()))
+                .collect(Collectors.toList());
+    }
+
+    private static List<Flight> conditionTwo(List<Flight> testList) {
+        return testList.stream()
+                .filter(flight -> flight.getSegments()
+                        .stream().allMatch(segment -> segment.getDepartureDate().isBefore(segment.getArrivalDate())))
+                .collect(Collectors.toList());
+    }
+
 }
